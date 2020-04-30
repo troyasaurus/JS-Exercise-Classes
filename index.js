3a94bcc4448ce17d34a227b07a0275b04896cbf8
Troy mangrum <tmmangrum@gmail.com>
2020-04-29 22:47:05 -0500

    made some beginning changes

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+}
+toString(){
+  return `${this.name}, ${this.age}`;
+}
+  eat(food){
+    if(this.stomach.length < 10){
+      this.stomach.push(food)
+    }
+  }
+  poop(){
+    return this.stomach = []
+  }
 }
 
 /*
